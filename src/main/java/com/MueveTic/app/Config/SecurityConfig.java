918e3fe45fbb47f8e3d0a1e91409964d97d19424
MedinaMV <valderasalejandro53@gmail.com>
2023-11-30 02:36:58 +0100

    Arreglado fallo URL

--- src/main/java/com/MueveTic/app/Config/SecurityConfig.java
+++ src/main/java/com/MueveTic/app/Config/SecurityConfig.java
@@ -86,7 +86,7 @@ public class SecurityConfig {
 	@Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("https://muevetic-frontend2023.web.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
